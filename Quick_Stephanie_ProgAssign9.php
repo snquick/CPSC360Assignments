e549e2fcb210f31fc4a3da0641407f596f3caa00
Stephanie Quick <35506354+snquick@users.noreply.github.com>
2020-04-28 20:55:00 -0500

    Add files via upload

---
+++ Quick_Stephanie_ProgAssign9.php
@@ -0,0 +1,23 @@
+
+<html>
+	<body>
+	<?php
+	/* 	Author: Stephanie Quick
+   		Course: Applied Programming Languages
+   		Program: PHP Basics
+   		Date: April 28th, 2020
+	*/
+    // Variables
+    $start = 3;
+	$end = 5;
+	$sum = 0;
+	
+	// for loop that adds to the sum value
+	for ($i = $start; $i <= $end; $i++) {
+		$sum += $i;
+	}
+	// echos sum final value
+	echo $sum;
+	?>
+	</body>
+</html>
